ba0f6527c522f90bf5090901f700fc7a2b6bfa8d
Robert Faulk <robertf@spiceworks.com>
2015-08-12 14:57:45 -0500

    surrounded html2canvas with document.ready

--- js/game.js
+++ js/game.js
@@ -70,14 +70,16 @@ Game.prototype.initialise = function(gameCanvas) {
   this.gameCanvas = gameCanvas;
 
   if(self == top){
-    console.log('self == top');
-    $('.ticket').remove();
-    console.log('removed .ticket');
-    html2canvas($('.mock_ticket')[0], { onrendered: function(canvas){
-      console.log('HI');
-      box = canvas.getContext('2d');
-      $('.mock_ticket').remove();
-    }});
+    $(document).ready(function(){
+      console.log('self == top');
+      $('.ticket').remove();
+      console.log('removed .ticket');
+      html2canvas($('.mock_ticket')[0], { onrendered: function(canvas){
+        console.log('HI');
+        box = canvas.getContext('2d');
+        $('.mock_ticket').remove();
+      }});
+    }
   }
   else{
     $('.mock_ticket').remove();
